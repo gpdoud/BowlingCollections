b25ea97c451116cce87d5fa270ee3d438b6788a5
Greg Doud <gpdoud@gmail.com>
2021-07-06 14:15:15 -0400

    Add project files.

---
+++ BowlingCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BowlingCollections {
+    class Program {
+        static void Main(string[] args) {
+
+            var game = new Bowling();
+            Console.WriteLine($"Game score is {game.Bowl()}");
+
+        }
+    }
+}
